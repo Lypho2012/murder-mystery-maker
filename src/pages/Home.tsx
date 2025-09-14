b7369fe88154307d5e1b4a7a05cb321adc8e112f
Cindy Zhang <xinyuz62@uci.edu>
2025-09-14 11:59:56 -0700

    setup home screen and murder board

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import "./Home.css"
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+    const navigate = useNavigate();
+    return (
+        <div id="home-div">
+            <p id="home-title">Murder Mystery Maker</p>
+            <button className="home-button" onClick={()=> {navigate("/murder-board")}}>New</button>
+            <button className="home-button">Load</button>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/MurderBoard.tsx
@@ -0,0 +1,10 @@
+import "./MurderBoard.css"
+function MurderBoard() {
+  return (
+    <div id="board-div">
+      <input type="text" id="board-title" placeholder="Murder Mystery Title"/>
+    </div>
+  )
+}
+
+export default MurderBoard
